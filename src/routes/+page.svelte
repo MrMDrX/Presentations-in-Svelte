ae279560e4a4c9261cf6eff023d988ceffdb4791
MrMDrX <geek.4tech007@gmail.com>
2024-03-25 00:34:53 +0000

    feat: :lipstick: add tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="bg-sky-700 px-4 py-2 text-white font-bold sm:px-8 sm:py-3">Presentations in SvelteKit</h1>
