5d8a7a4035cb1e7d22cae1a6603913f019379a3c
MrMDrX <geek.4tech007@gmail.com>
2024-03-27 00:15:17 +0000

    feat: 🎉 add code component

--- src/lib/deck/presentation.svelte
+++ src/lib/deck/presentation.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Slide from "./slide.svelte";
+  import Code from "./code.svelte";
 </script>
 
 <Slide>Horizontal Slide</Slide>
@@ -25,4 +26,22 @@
     <li>Solid</li>
     <li>Vue</li>
   </ul>
+</Slide>
+
+<Slide animate>
+  <Code id="code" lines>
+    {`
+			function love() {}
+	 `}
+  </Code>
+</Slide>
+
+<Slide animate>
+  <Code id="code" lines="2|1-3">
+    {`
+			function love() {
+				console.log('Svelte')
+			}
+	 `}
+  </Code>
 </Slide>
